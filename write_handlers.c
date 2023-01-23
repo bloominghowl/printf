effb694c3f4244dc9cf31e4e7f93948e92706995
Borngod <117746948+Borngod@users.noreply.github.com>
2023-01-23 17:39:05 +0000

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -12,7 +12,6 @@
  *
  * Return: Number of chars printed.
  */
-
 int handle_write_char(char c, char buffer[],
 	int flags, int width, int precision, int size)
 { /* char is stored at left and paddind at buffer's right */
@@ -58,7 +57,6 @@ int handle_write_char(char c, char buffer[],
  *
  * Return: Number of chars printed.
  */
-
 int write_number(int is_negative, int ind, char buffer[],
 	int flags, int width, int precision, int size)
 {
@@ -93,7 +91,6 @@ int write_number(int is_negative, int ind, char buffer[],
  *
  * Return: Number of printed chars.
  */
-
 int write_num(int ind, char buffer[],
 	int flags, int width, int prec,
 	int length, char padd, char extra_c)
